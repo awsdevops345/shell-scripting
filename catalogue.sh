1e7d4319823df516bd8617f4c22340446a15a14e
awsdevops345 <naveena838693@gmail.com>
2023-12-25 11:13:53 +0530

    saving changes

--- catalogue.sh
+++ catalogue.sh
@@ -86,7 +86,7 @@ systemctl start catalogue &>> $LOGFILE
 
 VALIDATE $? "Starting catalogue"
 
-cp /home/centos/Shell-scripting/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/shell-scripting/mongo.repo /etc/yum.repos.d/mongo.repo
 
 VALIDATE $? "copying mongodb repo"
 
