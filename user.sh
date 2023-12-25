78238f9f6e7c85ca60b4d760bb082d96d9c61e42
awsdevops345 <naveena838693@gmail.com>
2023-12-25 15:41:53 +0530

    saving changes

---
+++ user.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGO_HOST=mongo.learnaws.online
+
+MONGO_PORT=27017
+DATABASE_NAME=user
+COLLECTION_NAME=users
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current NodeJS"
+
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+
+VALIDATE $? "Enabling NodeJS:18"
+
+dnf install nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "Installing NodeJS:18"
+
+id roboshop #if roboshop user does not exist, then it is failure
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip
+
+cd /app 
+
+unzip -o /tmp/user.zip
+
+npm install 
+
+VALIDATE $? "Installing dependencies"
+
+cp /home/centos/shell-scripting/user.service /etc/systemd/system/user.service &>> $LOGFILE
+
+VALIDATE $? "Copying user service file"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "user daemon reload"
+
+systemctl enable user &>> $LOGFILE
+
+VALIDATE $? "Enable user"
+
+systemctl start user &>> $LOGFILE
+
+VALIDATE $? "Starting user"
+
+cp /home/centos/shell-scripting/mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB client"
+
+QUERY_RESULT=$(mongo --quiet --host $MONGO_HOST --port $MONGO_PORT $DATABASE_NAME --eval "db.$COLLECTION_NAME.count()")
+
+# Check the result of the query
+if [ "$QUERY_RESULT" -gt 0 ]; then
+    echo "MongoDB collection '$COLLECTION_NAME' in database '$DATABASE_NAME' has data."
+    exit 1;
+else
+    echo "MongoDB collection '$COLLECTION_NAME' in database '$DATABASE_NAME' is empty."
+
+    mongo --host $MONGDB_HOST </app/schema/user.js &>> $LOGFILE
+
+    VALIDATE $? "Loading catalouge data into MongoDB" 
+fi
+
+
